f19352eee407c2db82ee76801c6de3fe6ad4b9d8
mo8047 <itshassan45@gmail.com>
2025-07-27 23:02:56 +0400

    blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,87 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+# Basic program:
+
+# compile1 = random.choice(cards)
+# compile2 = random.choice(cards)
+# compile3 = random.choice(cards)
+# compile4 = random.choice(cards)
+#
+# user.append(compile1)
+# user.append(compile3)
+#
+# computer.append(compile2)
+# computer.append(compile4)
+
+# User CLI:
+
+
+u_input = input("Would you like to play?: (y/n)")
+while u_input == "y":
+    from art import logo
+    print(logo)
+    user = []
+    computer = []
+    #Backend
+    compile1 = random.choice(cards)
+    compile2 = random.choice(cards)
+    compile3 = random.choice(cards)
+    compile4 = random.choice(cards)
+    #compile5 = first user draw
+    #compile7 = second user draw
+    user.append(compile1)
+    user.append(compile3)
+
+    computer.append(compile2)
+    computer.append(compile4)
+    #sum print
+    sum_user = sum(user)
+    sum_computer = sum(computer)
+
+    print(f"User cards are {user} and the sum is {sum_user}")
+    print(f"Computer's first card is {computer[0]}")
+    another_draw = input("Would you like to draw another card: (y/n)")
+
+    if another_draw == "y":
+        compile5 = random.choice(cards)
+        user.append(compile5)
+        sum_user = sum(user)
+        print(f"Your cards are: {user} and the sum is :{sum_user}")
+
+        secod_draw = input("Would you like another draw: (y/n)")
+        if secod_draw == "y":
+            compile7 = random.choice(cards)
+            user.append(compile7)
+            sum_user = sum(user)
+            print(f"Your cards are: {user} and the sum is :{sum_user}")
+        elif secod_draw == "n":
+            print("Ok")
+    elif another_draw == "n":
+        print("Fine")
+
+
+
+#make sure computer is >17
+    while sum_computer < 17:
+        computer.append(random.choice(cards))
+        sum_computer = sum(computer)
+
+    if sum_user > 21:
+        print("You lost your total is greater than 21")
+        u_input = ("Play again?: (y/n)")
+        continue
+    elif sum_computer > 21:
+        print(f"User wins because the computer's sum is greater than 21 the cards are {user} and the sum is {sum_user}")
+    elif sum_user == sum_computer:
+        print("It's a draw")
+    elif sum_computer > sum_user:
+        print(f"Computer wins with a sum of {sum_computer}")
+    elif sum_user > sum_computer:
+        print(f"User wins with a sum of {sum_user}")
+
+    print(f"Computer {sum_computer, computer} User: {sum_user, user}")
+
+    u_input = input("Play again?: (y/n)")
+
